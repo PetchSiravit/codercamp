6e6c91d6005697cc2f91c2a9a8771c03c687a9c2
PetchSiravit <91052206+PetchSiravit@users.noreply.github.com>
2021-10-07 16:28:46 +0700

    Update 3.2.c

--- 3.2.c
+++ 3.2.c
@@ -4,26 +4,26 @@
 
 int main(void)
 {
-    int table[2][4];
+    int table[4][3];
     //string name = get_string("Enter name: ");
 
     for (int j = 0; j <= 2; j++)
     {
         //get ID
         int id = get_int("Enter ID: ");
-        table[j][0] = id;
+        table[0][j] = id;
 
         //get score
         for (int i = 1; i <= 3; i++)
         {
             int score = get_int("Score: ");
-            table[j][i] = score;
+            table[i][j] = score;
         }
     }
 
     //print
     for(int k=0;k<=2;k++)
     {
-        printf("id: %d => M: %d , S: %d , E: %d \n",table[k][0],table[k][1],table[k][2],table[k][3]);
+        printf("id: %d => M: %d , S: %d , E: %d \n",table[0][k],table[1][k],table[2][k],table[3][k]);
     }
 }
